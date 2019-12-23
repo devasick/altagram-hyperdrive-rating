60a7e6036d1c38dfe7b50533585eedab52b1ddae
Asick Ahamed <ashiq.it@gmail.com>
2019-12-23 18:07:05 +0100

    teeee

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import flask
-#import pandas as pd
+import requests
+import json
+from flask import jsonify
+BASE_URL = 'https://swapi.co/api/starships/?page=3'
+ 
 
 
 def create_app():
@@ -7,24 +11,35 @@ def create_app():
 
     @app.route('/', methods=['GET', 'POST'])
     def index():
-        """
-        Index page view handler.
-        :return: rendered index.html template
-        """
-        return flask.render_template('index.html')
+        global swapi_dict
+        url = BASE_URL
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+        data = requests.get(url, headers=headers).json()
+        
+        swapi_dict = {'starships':[], 'starships_unknown_hyperdrive':[]} 
+        
 
-    @app.route('/data', methods=['GET', 'POST'])
+        for result in data['results']:
+            
+            if result['hyperdrive_rating'] != 'unknown':
+                #amend value to starships array
+                swapi_dict['starships'].insert(0, {'name':result['name'],'hyperdrive':result['hyperdrive_rating']})
+                # sort by hyperdrive rating
+                swapi_dict['starships'] =  sorted(swapi_dict['starships'],key=lambda k: k['hyperdrive'], reverse=False)
+               
+            else:
+                #amend unknown hyperdrive to array
+                swapi_dict['starships_unknown_hyperdrive'].insert(0, {'name':result['name']})
+ 
+            
+        print(swapi_dict) 
+        return flask.render_template('index.html',data=swapi_dict)
+
+    @app.route('/results', methods=['GET', 'POST'])
     def data():
-        """
-        Data view handler
-        :return: JSON object of the data CSV file
-        """
-        data = pd.read_csv('task_data.csv')
-
-        context = {
-            'sensor_data': data.to_dict(orient='list')
-        }
-        return flask.jsonify(context)
+        index() 
+       
+        return swapi_dict
 
     return app
 
